9be7b066212d5749d3886c3d15940ea50f11a0e7
GannahMaged2004 <gannah.eltonsy04@gmail.com>
2025-09-06 05:56:57 +0300

    third commit

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,22 +1,26 @@
+import movieImg     from "../assets/movie-search.jpg";
+import eventxImg    from "../assets/eventx.jpg";
+import frameworkImg from "../assets/framework.jpg";
+
 export const projects = [
   {
     title: "Movie Search App",
-    img: "public/movie-search.jpg",
-    desc: "A React app to search for any movie you'd like. You can search by title, actors, genres, and you'll find it's description,director,actors etc...  .",
+    img: movieImg,
+    desc: "A React app to search for any movie you'd like...",
     live: "https://movie-search-app-one-blond.vercel.app",
     repo: "https://github.com/GannahMaged2004/Movie-Search-App",
   },
   {
     title: "GE EventX Studio",
-    img: "public/eventx.jpg",
-    desc: "Built with React & Node.js. Depending on who logs in admin/user it'll take you where you need to be. If you're an admin, you can create, edit, and delete events. If you're a user, you can view events and book them etc...",
+    img: eventxImg,
+    desc: "Built with React & Node.js. Depending on who logs in...",
     live: "https://ge-event-x-studio-git-main-gannahmaged2004s-projects.vercel.app",
     repo: "https://github.com/GannahMaged2004/GE-EventX-Studio",
   },
   {
     title: "First React Framework",
-    img: "public/framework.jpg",
-    desc: "Starter React portfolio with basic routing and components it doesn't really include much but it's a good starting point for anyone who wants to build their own portfolio.",
+    img: frameworkImg,
+    desc: "Starter React portfolio with basic routing and components...",
     live: "https://first-react-framework.vercel.app",
     repo: "https://github.com/GannahMaged2004/First-React-Framework",
   },
